74f240133364f247abcb0f654167f8914eb237b9
Denis <denkul87@gmail.com>
2020-06-13 00:18:14 +0300

    Создание класса Пользователь

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Имя пола
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создание нового пола
+        /// </summary>
+        /// <param name="name"></param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Пол не может отсутствовать", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
